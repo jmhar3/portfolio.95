15d6959c4c03ab696c67bbc799a778fcce19af2a
jmhar <jmhar@protonmail.com>
2022-03-01 14:07:43 +1100

    display message after submission

--- src/desktop/components/windows/Contact.js
+++ src/desktop/components/windows/Contact.js
@@ -1,5 +1,5 @@
 import Draggable from 'react-draggable';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { randomNumber } from '../../../Helpers';
 
 const Contact = ({
@@ -15,7 +15,8 @@ const Contact = ({
   setTop(randomNumber(10, 30))
   setLeft(randomNumber(10, 50))
  }, [])
- 
+
+ const [submitted, setSubmitted] = useState(false)
  const [values, setValues] = useState({
   sender: "",
   subject: "",
@@ -24,8 +25,13 @@ const Contact = ({
 
  const sendMessage = (e) => {
   e.preventDefault();
-  console.log(values)
+  console.log(values);
+
+  setSubmitted(true);
+  setTimeout(() => setSubmitted(false), 10000);
  }
+
+ useEffect(() => {setSubmitted(false)}, [])
 
  return (
   <Draggable handle=".contact">
@@ -64,61 +70,70 @@ const Contact = ({
       >&times;</button>
      </span>
     </span>
-    <form
-     className="window-body column"
-     onSubmit={sendMessage}
+    <span
+     className="window-body"
     >
-     <div className="row center">
-      <h5>To:</h5>
-      <a
-       href="mailto:jmhar@protonmail.com"
-       target="_blank"
-       className="rev-border"
+     {submitted ?
+      <>
+       <h3>Thank you!</h3>
+       <h4>I'll be in touch soon!</h4>
+      </> :
+      <form
+       className="column"
+       onSubmit={sendMessage}
       >
-       jmhar@protonmail.com
-      </a>
-     </div>
-     <div className="row center">
-      <h5>From:</h5>
-      <input
-       type="text"
-       name="sender"
-       className="rev-border"
-       value={values.sender} 
-       onChange={(e) => setValues({
-        ...values,
-        sender: e.target.value
-       })}
-      />
-     </div>
-     <div className="row center">
-      <h5>Subject:</h5>
-      <input
-       type="text"
-       name="subject"
-       className="rev-border"
-       value={values.subject} 
-       onChange={(e) => setValues({
-        ...values,
-        subject: e.target.value
-       })}
-      />
-     </div>
-     <div className="column center">
-      <textarea
-       name="message"
-       className="rev-border"
-       value={values.message} 
-       onChange={(e) => setValues({
-        ...values,
-        message: e.target.value
-       })}
-      />
-      <div className="right">
-       <button type="submit">Send</button>
-      </div>
-     </div>
-    </form>
+       <div className="row center">
+        <h5>To:</h5>
+        <a
+         href="mailto:jmhar@protonmail.com"
+         target="_blank"
+         className="rev-border"
+        >
+         jmhar@protonmail.com
+        </a>
+       </div>
+       <div className="row center">
+        <h5>From:</h5>
+        <input
+         type="text"
+         name="sender"
+         className="rev-border"
+         value={values.sender}
+         onChange={(e) => setValues({
+          ...values,
+          sender: e.target.value
+         })}
+        />
+       </div>
+       <div className="row center">
+        <h5>Subject:</h5>
+        <input
+         type="text"
+         name="subject"
+         className="rev-border"
+         value={values.subject}
+         onChange={(e) => setValues({
+          ...values,
+          subject: e.target.value
+         })}
+        />
+       </div>
+       <div className="column center">
+        <textarea
+         name="message"
+         className="rev-border"
+         value={values.message}
+         onChange={(e) => setValues({
+          ...values,
+          message: e.target.value
+         })}
+        />
+        <div className="right">
+         <button type="submit">Send</button>
+        </div>
+       </div>
+      </form>}
+    </span>
    </div>
   </Draggable >
  )
